50aa31fbfe7be6333cb307e8be4922884665ebf3
Sergiu Bonta <49238250+Sehige@users.noreply.github.com>
2022-07-19 00:32:51 +0300

    heroes go on cards now and decided on aspect ratio

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  //debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  _FavouriteState createState() => _FavouriteState();
+}
+
+class _FavouriteState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'UnMatched',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Heroes'),
@@ -26,23 +33,46 @@ class MyApp extends StatelessWidget {
 }
 
 Widget HeroGrid = GridView.builder(
-    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      crossAxisSpacing: 10.0,
-      mainAxisSpacing: 10.0,
-    ),
-    itemCount: 4,
-    itemBuilder: (BuildContext context, int index) {
-      //return Center(child: Text('Item $index'));
-      //return Center(child: Image.asset('images/square.png'));
-      return Container(
-        alignment: Alignment.center,
-        height: double.infinity,
-        decoration: BoxDecoration(
-            color: _list[index].color, borderRadius: BorderRadius.circular(4)),
-        child: Image.asset(_list[index].image),
-      );
-    });
+  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2, childAspectRatio: 0.5,
+    //crossAxisSpacing: 10.0,
+    //mainAxisSpacing: 10.0,
+  ),
+  itemCount: 4,
+  itemBuilder: (BuildContext context, int index) {
+    double dAspectRatio = 0.6; //zoom out 1.8, zoom in 0.6
+    int nNrOfClicks = 0;
+    //return Center(child: Text('Item $index'));
+    //return Center(child: Image.asset('images/square.png'));
+    return Card(
+      color: _list[index].color,
+      child: Column(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: dAspectRatio,
+            child: Image.asset(
+              _list[index].image,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              nNrOfClicks++;
+              if (nNrOfClicks % 2 == 0) {
+                dAspectRatio = 0.75;
+              } else {
+                dAspectRatio = 1.8;
+              }
+            },
+            child: AutoSizeText(
+              _list[index].name,
+            ),
+          ),
+        ],
+      ),
+    );
+  },
+);
 
 class PortraitItem {
   final String image;
